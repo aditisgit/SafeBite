39a8d50a3fbf5b2fabf4a593a4548427cb9fb2ae
ananyag595 <ananyagupta595@gmail.com>
2025-03-20 12:33:44 +0530

    added [] in lines 130 131

--- Deployment/app.py
+++ Deployment/app.py
@@ -127,8 +127,8 @@ with tab3:
 
     if st.button("Predict Safety", key="btn_s"):
         user_input = pd.DataFrame({
-                        'FoodGroupEncoded': foodgroup_encoder.transform(food_group_name)[0],
-                        'ContaminantEncoded': contaminant_encoder.transform(contaminant)[0],
+                        'FoodGroupEncoded': foodgroup_encoder.transform([food_group_name])[0],
+                        'ContaminantEncoded': contaminant_encoder.transform([contaminant])[0],
                         'LogResultValue':result_scaler.transform(np.array([[contaminant_quantity]]))[0][0]
         }, index = [0])
         safety_pred = safety.predict(user_input)
